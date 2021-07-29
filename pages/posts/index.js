a43eac55aa5b3d07d6bc775480d1292744bf7e83
thetooaung07 <thetooaung07@gmail.com>
2021-07-29 23:55:49 +0630

    Adding allpost page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,44 @@
-export default function AllPostsPage() {
+import AllPosts from "../../components/posts/all-posts";
 
+
+
+const Dummy_Posts = [
+   {
+     title: "Getting started with NextJS",
+     image: "getting-started-nextjs.png",
+     excerpt:
+       "NextJs is a production Fullstack framework for Reactjs - it supports both SSR and SSG ",
+     date: "2022-02-10",
+     slug: "getting-started-with-nextjs",
+   },
+   {
+     title: "Getting started with NextJS",
+     image: "getting-started-nextjs.png",
+     excerpt:
+       "NextJs is a production Fullstack framework for Reactjs - it supports both SSR and SSG ",
+     date: "2022-02-10",
+     slug: "getting-started-with-nextjs",
+   },
+   {
+     title: "Getting started with NextJS",
+     image: "getting-started-nextjs.png",
+     excerpt:
+       "NextJs is a production Fullstack framework for Reactjs - it supports both SSR and SSG ",
+     date: "2022-02-10",
+     slug: "getting-started-with-nextjs",
+   },
+   {
+     title: "Getting started with NextJS",
+     image: "getting-started-nextjs.png",
+     excerpt:
+       "NextJs is a production Fullstack framework for Reactjs - it supports both SSR and SSG ",
+     date: "2022-02-10",
+     slug: "getting-started-with-nextjs",
+   },
+ ];
+
+export default function AllPostsPage() {
+   return (
+      <AllPosts posts={Dummy_Posts}></AllPosts>
+   )
 }
