bfdd3c967a851b21bba254b20509123ae4bb6b17
thetooaung07 <thetooaung07@gmail.com>
2021-07-29 23:54:24 +0630

    add posts

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -5,9 +5,9 @@ export default function PostsGrid(props) {
   const { posts } = props;
 
   return (
-    <ul>
-      {posts.map((post, index) => (
-        <PostItem key={index}/>
+    <ul className={classes.grid}>
+      {posts.map((post) => (
+        <PostItem key={post.slug} post={post}/>
       ))}
     </ul>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,46 @@
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
 
+const Dummy_Posts = [
+  {
+    title: "Getting started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJs is a production Fullstack framework for Reactjs - it supports both SSR and SSG ",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs",
+  },
+  {
+    title: "Getting started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJs is a production Fullstack framework for Reactjs - it supports both SSR and SSG ",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs",
+  },
+  {
+    title: "Getting started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJs is a production Fullstack framework for Reactjs - it supports both SSR and SSG ",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs",
+  },
+  {
+    title: "Getting started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJs is a production Fullstack framework for Reactjs - it supports both SSR and SSG ",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs",
+  },
+];
+
 export default function HomePage() {
   return (
     <>
       <Hero></Hero>
-      <FeaturedPosts></FeaturedPosts>
+      <FeaturedPosts posts={Dummy_Posts} />
     </>
   );
 }
