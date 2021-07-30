9b3616103dc5b387695cb0f739fe01a8341a88fc
thetooaung07 <thetooaung07@gmail.com>
2021-07-30 17:47:35 +0630

    Rendering image from markdown to jsx

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,21 +1,58 @@
 import PostHeader from "./post-header";
-import classes from "./post-content.module.css"
+import classes from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
+import Image from "next/dist/client/image";
 
-const Dummy_Post = {
-  title: "Getting started with NextJS",
-  image: "getting-started-nextjs.png",
-  date: "2022-02-10",
-  slug: "getting-started-with-nextjs",
-  content: '# This is the first post'
-};
+// const Dummy_Post = {
+//   title: "Getting started with NextJS",
+//   image: "getting-started-nextjs.png",
+//   date: "2022-02-10",
+//   slug: "getting-started-with-nextjs",
+//   content: '# This is the first post'
+// };
 
-export default function PostContent() {
-   const imagePath = `/images/posts/${Dummy_Post.slug}/${Dummy_Post.image}`
+export default function PostContent(props) {
+  const { post } = props;
+
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     ></Image>
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
-      <PostHeader title={Dummy_Post.title} image={imagePath}></PostHeader>
-     <ReactMarkdown>{Dummy_Post.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath}></PostHeader>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,34 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostFiles } from "../../lib/posts-util";
 
-export default function PostDetailPage() {
-  return <PostContent></PostContent>;
+export default function PostDetailPage(props) {
+  return <PostContent post={props.post} />;
+}
+
+export function getStaticProps(context) {
+  const { slug } = context.params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFilenames = getPostFiles();
+
+  const slugs = postFilenames.map((filename) => filename.replace(/\.md$/, ""));
+
+  return {
+    paths: slugs.map((slug) => ({
+      params: {
+        slug: slug,
+      },
+    })),
+    fallback: false,
+  };
 }
