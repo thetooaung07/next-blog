5ffe377b6b5385516cbb6678cabff71a8e838cb0
thetooaung07 <thetooaung07@gmail.com>
2021-08-01 22:30:41 +0630

    Storing data in mongodb

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -24,10 +26,34 @@ export default function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
 
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://thetooaung:thetooaung@cluster0.uclkr.mongodb.net/next-blog?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res
+        .status(500)
+        .json({
+          message:
+            "Something went wrong on the server. Could not connect to the database",
+        });
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage._id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing Message failed!" });
+      return;
+    }
+    client.close();
     res.status(201).json({ message: "Success", result: newMessage });
   }
 
-  res.status(200).json({ message: "Success" });
+  // res.status(200).json({ message: "Success" });
 }
