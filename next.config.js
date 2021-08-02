694698bb79de7e84896b1a12eab822b90a894890
thetooaung07 <thetooaung07@gmail.com>
2021-08-02 11:48:11 +0630

    error fix in next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
