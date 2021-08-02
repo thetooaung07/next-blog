a7421833bd8469a71b01961fdc095412bc169fcc
thetooaung07 <thetooaung07@gmail.com>
2021-08-02 14:29:24 +0630

    Change env values to vercel

--- next.config.js
+++ next.config.js
@@ -1,23 +1,30 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+// You can embed environemt variable here for production
+// but better private your git repo since next.config.js will be visible in git repo
+
+/* const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
-        db_username: "thetooaung",
-        db_password: "thetooaung",
-        db_clustername: "cluster0",
-        db: "next-blog",
+        db_username: "lorem",
+        db_password: "lorem",
+        db_clustername: "lorem",
+        db: "lorem",
       },
     };
   }
   return {
     reactStrictMode: true,
     env: {
-      db_username: "thetooaung",
-      db_password: "thetooaung",
+      db_username: "lorem",
+      db_password: "lorem",
       db_clustername: "cluster0",
-      db: "next-blog",
+      db: "lorem",
     },
   };
+}; */
+
+module.exports = {
+  reactStrictMode: true,
 };
